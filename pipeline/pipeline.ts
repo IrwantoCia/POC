91c26450d29a9d66ae8eb38031191b8f36907fba
irwanto cia <irwantocia@gmail.com>
2023-04-17 14:57:54 +0700

    Initial commit

--- pipeline/pipeline.ts
+++ pipeline/pipeline.ts
@@ -1,26 +1,70 @@
 type Func<T, U> = (arg: T) => U | Promise<U>;
+type Fn = {
+    fn: Func<any, any>;
+    param: any;
+}
 
 export class Pipeline<T> {
     private readonly initialValue: T;
-    private fn: Func<any, any>[] = [];
+    private fnList: Fn[] = [];
 
     constructor(init: T) {
         this.initialValue = init;
     }
 
-    addFunc<U>(func: Func<T, U>): Pipeline<U> {
-        this.fn.push(func);
-        return this as unknown as Pipeline<U>;
+    addFunc<U>(func: Func<T | unknown, U>, overrideParam?: unknown) {
+        this.fnList.push({fn: func, param: overrideParam});
+        return this as unknown as Pipeline<U | unknown>;
     }
 
     async run() {
         let result = this.initialValue;
-        for (const func of this.fn) {
-            result = func(result);
-            if (result instanceof Promise) {
-                result = await result;
+        for (const func of this.fnList) {
+            if (func.param) {
+                result = await func.fn(func.param);
+                continue;
             }
+            result = func.fn(result);
         }
         return result;
-    }
+    };
 }
+
+type User = {
+        name: string;
+        age: number;
+    }
+
+type UserDto = {
+    name: string;
+}
+
+function findUserById(id: number): Promise<User> {
+    return Promise.resolve({name: 'John', age: 20});
+}
+
+function createUser(user: UserDto):User {
+    return {
+        name: user.name,
+        age: 20,
+    };
+}
+
+function isUserExist(user: User) {
+    return
+}
+
+const userDto: UserDto = {
+    name: 'John',
+}
+
+function test() {
+    const pipeline = new Pipeline<number>(1);
+    return pipeline
+        .addFunc(findUserById)
+        .addFunc(isUserExist)
+        .addFunc(createUser, userDto)
+        .run();
+}
+
+test().then(value => console.log(value));
